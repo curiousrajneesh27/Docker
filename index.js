a86d013fe371753026cb17908be97c3d1b6984d7
rajneesh verma <curiousrajneesh@2027gmail.com>
2025-12-02 00:24:42 +0530

    updated

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express  = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World i am rajneesh and i am a remte sooftware developer at africa based startup as a backend engineer and work on next js and ndoe js wirh typescript!');
 });
 
 app.listen(3000 );
 
 console.log("ENV VARIABLE");
+
 console.log(process.env.MY_ENV_VARIABLE);
