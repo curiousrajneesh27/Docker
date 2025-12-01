235ad5db82a1ada264850ff6505e4640146ced84
rajneesh verma <curiousrajneesh@2027gmail.com>
2025-12-01 22:09:04 +0530

    env updated

--- index.js
+++ index.js
@@ -6,23 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.get('/about', (req, res) => {
-    res.send('About Us');
-});
+app.listen(3000 );
 
-app.get('/contact', (req, res) => {
-    res.send('Contact Us');
-});
-
-
-app.get('/products', (req, res) => {
-    res.send('Our Products');
-});
-
-app.get('/services', (req, res) => {
-    res.send('Our Services');
-});
-
-app.listen(3000 ); () => {
-    console.log('Server is running on http://localhost:3000');
-};
+console.log("ENV VARIABLE");
+console.log(process.env.MY_ENV_VARIABLE);
