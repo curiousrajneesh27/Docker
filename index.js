ec2762d37e26bb526d46f16483eab30e6386f41a
rajneesh verma <curiousrajneesh@2027gmail.com>
2025-12-01 20:22:07 +0530

    Updated

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Us');
+});
+
+
+app.get('/products', (req, res) => {
+    res.send('Our Products');
+});
+
+app.get('/services', (req, res) => {
+    res.send('Our Services');
+});
+
 app.listen(3000 ); () => {
     console.log('Server is running on http://localhost:3000');
 };
